1b2926cf97ddf2012cb2eaa2b0b2ce3ccdf973a9
jkurkure <117855264+jkurkure@users.noreply.github.com>
2025-02-17 16:18:26 +0000

    try to fix subprocess detachment

--- runner.py
+++ runner.py
@@ -1,3 +1,5 @@
 import subprocess
-subprocess.Popen(['python3.11', '/home/public/kairy/App.pyw'])
+subprocess.Popen(['python3.11', '/home/public/kairy/App.pyw'],stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,  # needed for the next line to be sensible
+    stderr=subprocess.STDOUT)
 print('Bringka is launched')
