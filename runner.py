754b35c755b97c2731d6f6c9f28c35c21db8e54a
jkurkure <117855264+jkurkure@users.noreply.github.com>
2025-02-17 11:36:53 +0000

    go back to what works

--- runner.py
+++ runner.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3.11
+
 import subprocess
-from nicegui import ui
 subprocess.Popen(['python3.11', '/home/public/kairy/App.pyw'])
-ui.label('Bringka has been launched')
-ui.run(port=8090, dark=True)
+print('Bringka is launched')
