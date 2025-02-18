6520089ff20eca61933dcac678e2b967585cf0da
jkurkure <117855264+jkurkure@users.noreply.github.com>
2025-02-18 10:28:11 +0000

    somehow stop daemon from failing

--- runner.py
+++ runner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3.11
-import subprocess, env
+import subprocess, env, time
 
 s = subprocess.Popen([env.PYRUNNER, 'App.pyw'], stdin=subprocess.PIPE,
     stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
@@ -7,8 +7,10 @@ s = subprocess.Popen([env.PYRUNNER, 'App.pyw'], stdin=subprocess.PIPE,
 print(f'{env.APP_NAME} is launched')
 
 while True:
+    print(f'Still alive at {time.ctime()}')
+
     output = s.stdout.readline() # type: ignore
     if output == '' and s.poll() is not None:
-        break
+        time.sleep(0.1)
     if output:
         print(output.strip())
