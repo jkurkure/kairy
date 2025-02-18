42fc2f55b0710476923a84cb7425befd5d3217a7
jkurkure <117855264+jkurkure@users.noreply.github.com>
2025-02-18 10:52:07 +0000

    alive logging

--- runner.py
+++ runner.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3.11
-import subprocess, env, time
+import subprocess, env, time, random
 
 pop = subprocess.Popen([env.PYRUNNER, 'App.pyw'])
 print(f'{env.APP_NAME} is launched')
 
 while pop.poll() is None:
-    time.sleep(0.1)
+    time.sleep(random.uniform(1/50, 2))
+    print(f'{env.APP_NAME} is running at {time.ctime()}')
 
 else:
     print(f'{env.APP_NAME} is terminated')
