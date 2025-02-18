0c9179b3d484b1f6221575532cb992115ac39028
jkurkure <117855264+jkurkure@users.noreply.github.com>
2025-02-18 10:09:40 +0000

    make runner provide output constantly

--- runner.py
+++ runner.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3.11
 import subprocess, env
-subprocess.Popen(['python3.11', '/home/public/kairy/App.pyw'],stdin=subprocess.PIPE,
+s = subprocess.Popen(['python3.11', '/home/public/kairy/App.pyw'],stdin=subprocess.PIPE,
     stdout=subprocess.PIPE,  # needed for the next line to be sensible
     stderr=subprocess.STDOUT)
 print(f'{env.APP_NAME} is launched')
+
+while True:
+    try:
+        line = s.stdout.readline().decode('utf-8').strip() # type: ignore
+        if line == '':
+            break
+        print(line)
+    except KeyboardInterrupt:
+        break
