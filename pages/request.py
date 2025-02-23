32376701e891f85794f356c0d69c454c78e4ddf1
jkurkure <117855264+jkurkure@users.noreply.github.com>
2025-02-23 15:01:09 +0000

    lint froms into imports

--- pages/request.py
+++ pages/request.py
@@ -1,35 +1,28 @@
 import base64
-from datetime import datetime
-from functools import partial
+import datetime
 import uuid
-from nicegui import ElementFilter, ui, app, events
-from utils import (
-    database,
-    dateCheck,
-    section,
-    fieldType,
-    Form,
-    unique,
-    phones,
-    addresses,
-)
-from .join import formlabel
+import nicegui
+import nicegui.events as events
+import utils
+import utils.addresses as addresses
+import utils.phones as phones
+import pages.join as join
 import env
 
 
 address_autocomplete = []
 
-if database.getTable("Items") is None:
-    database.newTable(
+if utils.database.getTable("Items") is None:
+    utils.database.newTable(
         "Items", "id", "requester", "name", "from", "to", "date", "price", "image"
     )
 
 
 def location(record, formValidCheck):
-    with ui.input("Location", autocomplete=address_autocomplete) as location:
+    with nicegui.ui.input("Location", autocomplete=address_autocomplete) as location:
         with location.add_slot("append"):
-            with ui.menu().props("no-parent-event") as menu:
-                section("Find your drop-off point")
+            with nicegui.ui.menu().props("no-parent-event") as menu:
+                utils.section("Find your drop-off point")
 
                 def findPlace(e):
                     lat, lng = addresses.lookFor(e.sender.value)
@@ -37,13 +30,13 @@ def location(record, formValidCheck):
                         m.center = (lat, lng)
                         m.zoom = 14
 
-                with ui.row():
-                    with ui.input().on("blur", findPlace) as search:
+                with nicegui.ui.row():
+                    with nicegui.ui.input().on("blur", findPlace) as search:
                         search.classes("no-form")
                         with search.add_slot("prepend"):
-                            search_icon = ui.icon("search").classes("cursor-pointer")
+                            search_icon = nicegui.ui.icon("search").classes("cursor-pointer")
 
-                m = ui.leaflet(
+                m = nicegui.ui.leaflet(
                     center=addresses.getCenter(phones.where(record["country"])), zoom=10
                 )  # type: ignore
 
@@ -66,31 +59,31 @@ def location(record, formValidCheck):
 
                 m.on("map-click", handle_click)
 
-                with ui.row().classes("justify-end"):
-                    ui.button("Close", on_click=menu.close).props("flat")
+                with nicegui.ui.row().classes("justify-end"):
+                    nicegui.ui.button("Close", on_click=menu.close).props("flat")
 
-            ui.icon("pin_drop").on("click", menu.open).classes("cursor-pointer")
+            nicegui.ui.icon("pin_drop").on("click", menu.open).classes("cursor-pointer")
 
 
 def show():
     photo = ""
 
-    if "logIn" not in app.storage.user:
-        ui.navigate.to("/app/join")
+    if "logIn" not in nicegui.app.storage.user:
+        nicegui.ui.navigate.to("/app/join")
 
     else:
-        i = app.storage.user["logIn"]
-        record = database.getTable("Users").iloc[i]  # type: ignore
+        i = nicegui.app.storage.user["logIn"]
+        record = utils.database.getTable("Users").iloc[i]  # type: ignore
 
-        setattr(form := Form(), "valid", False)
+        setattr(form := utils.Form(), "valid", False)
 
-        section("Tell us more!")
+        utils.section("Tell us more!")
 
-        with ui.grid(columns=2):
-            with ui.element("div").classes("p-2 bg-orange-100"):
-                formlabel("Item name: ")
-            with ui.element("div").classes("p-2 bg-blue-100"):
-                ui.input(
+        with nicegui.ui.grid(columns=2):
+            with nicegui.ui.element("div").classes("p-2 bg-orange-100"):
+                join.formlabel("Item name: ")
+            with nicegui.ui.element("div").classes("p-2 bg-blue-100"):
+                nicegui.ui.input(
                     validation=lambda value: (
                         "Only letters in your item's name please"
                         if not value.replace(" ", "").isalpha()
@@ -98,71 +91,71 @@ def show():
                     )
                 ).props("rounded outlined dense")
 
-            with ui.element("div").classes("p-2 bg-orange-100"):
-                formlabel("Where can it be picked up?")
-            with ui.element("div").classes("p-2 bg-blue-100"):
+            with nicegui.ui.element("div").classes("p-2 bg-orange-100"):
+                join.formlabel("Where can it be picked up?")
+            with nicegui.ui.element("div").classes("p-2 bg-blue-100"):
                 location(record, lambda e: formValidCheck(e))
 
-            with ui.element("div").classes("p-2 bg-orange-100"):
-                formlabel("Where do you want it delivered?")
-            with ui.element("div").classes("p-2 bg-blue-100"):
+            with nicegui.ui.element("div").classes("p-2 bg-orange-100"):
+                join.formlabel("Where do you want it delivered?")
+            with nicegui.ui.element("div").classes("p-2 bg-blue-100"):
                 location(record, lambda e: formValidCheck(e))
 
-            with ui.element("div").classes("p-2 bg-orange-100"):
-                formlabel("When do you want it delivered?")
-            with ui.element("div").classes("p-2 bg-blue-100"):
-                with ui.input("Date") as date:
-                    with ui.menu().props("no-parent-event") as menu:
-                        with ui.date().bind_value(date).on_value_change(
+            with nicegui.ui.element("div").classes("p-2 bg-orange-100"):
+                join.formlabel("When do you want it delivered?")
+            with nicegui.ui.element("div").classes("p-2 bg-blue-100"):
+                with nicegui.ui.input("Date") as date:
+                    with nicegui.ui.menu().props("no-parent-event") as menu:
+                        with nicegui.ui.date().bind_value(date).on_value_change(
                             lambda e: formValidCheck(e)
                         ):
-                            with ui.row().classes("justify-end"):
-                                ui.button("Close", on_click=menu.close).props("flat")
+                            with nicegui.ui.row().classes("justify-end"):
+                                nicegui.ui.button("Close", on_click=menu.close).props("flat")
                     with date.add_slot("append"):
-                        ui.icon("edit_calendar").on("click", menu.open).classes(
+                        nicegui.ui.icon("edit_calendar").on("click", menu.open).classes(
                             "cursor-pointer"
                         )
 
-            with ui.element("div").classes("p-2 bg-orange-100"):
-                formlabel("How much are you offering?")
-            with ui.element("div").classes("p-2 bg-blue-100"):
-                ui.number(min=2.50, max=100, step=0.01, prefix="SG $")
+            with nicegui.ui.element("div").classes("p-2 bg-orange-100"):
+                join.formlabel("How much are you offering?")
+            with nicegui.ui.element("div").classes("p-2 bg-blue-100"):
+                nicegui.ui.number(min=2.50, max=100, step=0.01, prefix="SG $")
 
-        fields = [f for f in list(ElementFilter(kind=fieldType)) if "no-form" not in f.classes]  # type: ignore
+        fields = [f for f in list(nicegui.ElementFilter(kind=utils.fieldType)) if "no-form" not in f.classes]  # type: ignore
 
         def formValidCheck(_):
-            form.valid = (False not in [field.value not in [None, ""] and not (field.validation and field.validation(field.value)) for field in fields]) and dateCheck(date.value, allow_yrs=range(datetime.now().year, datetime.now().year + env.MAX_ADVANCE_YRS + 1))  # type: ignore
+            form.valid = (False not in [field.value not in [None, ""] and not (field.validation and field.validation(field.value)) for field in fields]) and utils.dateCheck(date.value, allow_yrs=range(datetime.datetime.now().year, datetime.datetime.now().year + env.MAX_ADVANCE_YRS + 1))  # type: ignore
 
         [field.on("change", formValidCheck) for field in fields]
 
-        section("Attach a photo")
+        utils.section("Attach a photo")
 
         def setPhoto(e):
             nonlocal photo
-            ui.notify(f"Uploaded {e.name}")
+            nicegui.ui.notify(f"Uploaded {e.name}")
             rawData = base64.b64encode(e.content.read())
 
             photo = f"data:{e.type};base64,{rawData.decode()}"
 
-        ui.upload(
+        nicegui.ui.upload(
             on_upload=setPhoto,
-            on_rejected=lambda: ui.notify("Profile Picture is maximum 4.5MB!"),
+            on_rejected=lambda: nicegui.ui.notify("Profile Picture is maximum 4.5MB!"),
             max_file_size=4_500_000,
             max_files=1,
         ).classes("max-w-full")
 
         def listItem(e):
             if form.valid:  # type: ignore
-                database.addRow(
+                utils.database.addRow(
                     "Items",
-                    *unique(
+                    *utils.unique(
                         [uuid.uuid4(), record["username"]]
                         + [field.value for field in fields]
                         + [photo]
                     ),
                 )
-                ui.navigate.to("/app/users")
+                nicegui.ui.navigate.to("/app/users")
 
-        ui.button("List").props("rounded outlined").bind_enabled_from(form, "valid").on(
+        nicegui.ui.button("List").props("rounded outlined").bind_enabled_from(form, "valid").on(
             "click", listItem
         )
